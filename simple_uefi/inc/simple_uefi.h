0286c40ec43a4f481fb3d852d031e293eec8ef24
Rythorndoran <85108376+Rythorndoran@users.noreply.github.com>
2022-09-17 23:36:40 +0800

    Add files via upload

---
+++ simple_uefi/inc/simple_uefi.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+extern "C"
+{
+#include "efi.h" 
+}
---
+++ simple_uefi/src/simple_uefi.cpp
@@ -0,0 +1,19 @@
+﻿// simple_uefi.cpp: 定义应用程序的入口点。
+//
+
+#include "simple_uefi.h"
+
+EFI_STATUS EfiMain(EFI_HANDLE ImageHandle [[maybe_unused]], EFI_SYSTEM_TABLE* SystemTable)
+{
+	uint64_t key_event;
+
+	SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
+	
+	SystemTable->ConOut->SetAttribute(SystemTable->ConOut, EFI_GREEN);
+
+	SystemTable->ConOut->OutputString(SystemTable->ConOut, reinterpret_cast<CHAR16*>(const_cast<wchar_t*>(L"Hello World!\n")));
+
+	SystemTable->BootServices->WaitForEvent(1, &SystemTable->ConIn->WaitForKey, &key_event);
+
+	return EFI_SUCCESS;
+}
